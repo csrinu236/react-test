46ba19bea760c2b737b614b0845a4642d64b298c
Chenna.Sreenu <csrinu236@gmail.com>
2023-10-09 18:14:52 +0530

    added tizen webos logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:8888/api/index', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing LG & Samsung Deployment</h1>
+      <Calculator></Calculator>
     </div>
   );
 }
