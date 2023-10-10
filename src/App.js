a4dc6269e9d9e2bf57a14597e50934af81c9943b
Chenna.Sreenu <csrinu236@gmail.com>
2023-10-10 16:13:59 +0530

    removed fetch api

--- src/App.js
+++ src/App.js
@@ -3,21 +3,6 @@ import './App.css';
 import Calculator from './components/Calculator';
 
 function App() {
-  useEffect(() => {
-    fetch('http://localhost:8888/api/index', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((resp) => {
-        return resp.json();
-      })
-      .then((data) => {
-        console.log(data);
-      });
-  }, []);
-
   return (
     <div className="App">
       <h1>Testing LG & Samsung Deployment</h1>
